0edd11f8ba978d0d4adc8b3c40fe5b4931b2308f
Abdallah Magdy <abdullah.mahmoud02@eng-st.cu.edu.eg>
2023-11-10 16:20:47 +0200

    Start authentication

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,5 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function RootPage() {
   return (
-    <div>Root Page (Protected)</div>
-  )
+    <div>
+      <UserButton />
+    </div>
+  );
 }
